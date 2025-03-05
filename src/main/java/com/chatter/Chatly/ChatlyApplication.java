be2a96466f8ea7aa5735b24014b0e1a31c738037
zeroOne0012 <012012012il@gmail.com>
2025-03-05 14:21:07 +0900

    first commit

---
+++ src/main/java/com/chatter/Chatly/ChatlyApplication.java
@@ -0,0 +1,13 @@
+package com.chatter.Chatly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatter/Chatly/ChatlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatter.Chatly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
