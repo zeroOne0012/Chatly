bf5de0c553df3f8eb62c67a594548f33bc699265
zeroone0012 <012012012il@gmail.com>
2025-03-22 17:14:28 +0900

    (user)

--- src/main/java/com/chatter/Chatly/entity/User.java
+++ src/main/java/com/chatter/Chatly/entity/User.java
@@ -1,7 +1,10 @@
 package com.chatter.Chatly.entity;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
 
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -16,7 +19,7 @@ public class User {
     private String id;
 
     @Column
-    private String password; // 암호화된 비밀번호 저장
+    private String password; // 암호화
     @Column
     private String nickname;
     @Column(unique = true)
@@ -26,6 +29,10 @@ public class User {
     
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<JoinedUser> channels = new ArrayList<>();
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
     public User(String id, String password, String nickname, String email) {
         this.id = id;
