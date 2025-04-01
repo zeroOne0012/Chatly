77470e80f2d3beafcb465a5c31f0e446185e79a9
zeroOne0012 <012012012il@gmail.com>
2025-04-02 07:10:03 +0900

    DTO-NoArgsConstructor

--- src/main/java/com/chatter/Chatly/dto/ChannelMemberRequestDto.java
+++ src/main/java/com/chatter/Chatly/dto/ChannelMemberRequestDto.java
@@ -2,14 +2,20 @@ package com.chatter.Chatly.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChannelMemberRequestDto {
+    // @JsonProperty("channel_id")
     private Long channelId;
+    // @JsonProperty("member_id")
     private List<String> memberId;
 }
